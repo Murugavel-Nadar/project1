59934fdd007f4f6492f09778c4ebd0dc3bdfbc44
Murugavel-Nadar <vnadar42@gmail.com>
2025-07-19 18:35:03 +0530

    js add

--- script.js
+++
@@ -1,8 +0,0 @@
-// var my_firstName = "Velmurugan"; - working
-// var _myLastName$ = "Velmurugan"; - working
-// var 123myAge = "Velmurugan"; - not working
-// var $cityName = "Velmurugan"; - working
-// var my@Email = "Velmurugan"; - not working
-console.log (my@Email)
-
-// data type
